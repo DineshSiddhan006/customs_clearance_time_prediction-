1b5e027bc4da71c4f668dd48c88ce512a8cf1f5b
DineshSiddhan006 <dineshkumar.j@siddhanintelligence.com>
2026-06-09 10:21:27 +0530

    Create company_mapping.py

---
+++ company_mapping.py
@@ -0,0 +1,9 @@
+# company_mapping.py
+# Maps Special Handling Codes to specific enterprise profiles
+commodity_companies = {
+    "COL": ["Saja Pharma", "Tabuk Pharmaceuticals", "Saudi Chemical"],
+    "VAL": ["Al-Falak Electronic", "Jarir Marketing", "Zain Logistics"],
+    "GEN": ["Almarai Company", "Nadec Logistics", "General Trading Corp"],
+    "CRT": ["Saja Pharma", "Tabuk Pharmaceuticals"],
+    "PER": ["Almarai Company", "Nadec Logistics", "Alsafi Danone"]
+}
